288497466b71e808e04568d81973d882061e6255
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-12 00:44:42 -0500

    sets up tiles

--- js/main.js
+++ js/main.js
@@ -1,57 +1,11 @@
-class App {
-    constructor(){
-        this.moves = "";
-        this.render();
-        //modal for stretch
-    }
+const app = document.getElementById("app");
 
-    render(){
-        let app = document.getElementById("myApp");
-        this.buildHeader(app);
-        this.players(app);
-    }
-
-    generateHTML(type, classes, parent, text = "", id) {
-        const element = document.createElement(type);
-        element.className = (classes);
-        element.innerText = (text);
-        element.id = id;
-        parent.appendChild(element);
-        return element;
-    }
-
-    buildHeader(myApp) {
-        let container = this.generateHTML("div", "container", myApp);
-        // this row is specific to this header
-        let row = this.generateHTML("div", "row", container);
-        this.generateHTML("h1", "header text-center", row, "TicTacToe");
-    }
-
-    players(myApp) {
-        let container = this.generateHTML("div", "container", myApp);
-        // this row is specific to the players
-        let row = this.generateHTML("div", "row", container);
-        // this column is specific to the row that has players
-        let col = this.generateHTML("div", "col-6", row);
-        this.generateHTML("div", "player text-center top-0 start-0", col, "PlayerX");
-        // just call the row already created
-        col = this.generateHTML("div", "col-6", row);
-        this.generateHTML("div", "player text-center top-0 end-0", col, "PlayerO");
-    }
-
-}
-
-class Grid {
-    constructor(){
-        this.grid = [];
-    }
-}
+const players = [];
 
 class Tile {
-    constructor(){
-        this.tile = "";
+    constructor(id, tileText) {
+        this.id = id;
+        this.tileText = tileText;
+        this.clicked = false;
     }
 }
-
-let a = new App();
-console.log(a);
